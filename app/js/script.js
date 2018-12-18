b66c36705460582998c066980714c0e26b662c31
Vyacheslav <mister.slaus@gmail.com>
2018-12-18 12:10:57 +0200

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,109 @@
+//Scripts
+
+//Back to top button
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
+// scroll body to 0px on click
+$('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+        scrollTop: 0
+    }, 800);
+    return false;
+});
+
+//Carousel responsive
+$(function () {
+
+    var jcarousel = $('.jcarousel');
+
+    jcarousel
+        .on('jcarousel:reload jcarousel:create', function () {
+            var carousel = $(this),
+                width = carousel.innerWidth();
+
+            if (width >= 991) {
+                width = width / 2;
+            } else if (width >= 767) {
+                width = width;
+            }
+
+            carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular',
+            animation: {
+                duration: 1000,
+                speed: 1000,
+                easing: 'linear',
+                complete: function () {
+                }
+            }
+        })
+        .jcarouselAutoscroll({
+            interval: 8000,
+            target: '+=1',
+            autostart: true,
+        })
+        .on('mouseover', function (e) {
+            $(this).jcarouselAutoscroll('stop');
+        })
+        .on('mouseout', function (e) {
+            $(this).jcarouselAutoscroll('start');
+        });
+
+    $('.jcarousel-control-prev')
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .jcarouselControl({
+            target: '+=1'
+        });
+
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('active');
+        })
+        .on('click', function (e) {
+            e.preventDefault();
+        })
+        .jcarouselPagination({
+            perPage: 1,
+            item: function (page) {
+                return '<a href="#' + page + '">' + page + '</a>';
+            }
+        });
+
+//Match Height
+        $('.item').matchHeight({
+            byRow: true,
+            property: 'height',
+            target: null,
+            remove: false
+        });
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+});
+
+$(".btn-search").click(function (e) {
+    e.preventDefault();
+    $(this).hide();
+    $(".form-search").show();
+});
+
+$(document).mouseup(function (e) {
+    $(".form-search").has(e.target).length === 0 ? $(".btn-search").show() && $(".form-search").hide() : $(this);
+});
+
+
